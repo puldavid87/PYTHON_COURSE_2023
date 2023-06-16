3da4739367e659264b38c2e6c5c96c19d6b4c734
puldavid87 <puldavid87@gmail.com>
2023-06-16 20:12:07 +0200

    Unit5

---
+++ INDEX/4_Data_collections/2_Sets.py
@@ -0,0 +1,12 @@
+set_1 = set()
+set_1 = {1,2,3,4}
+print(set_1)
+set_1.add(4)
+print(set_1)
+set_1.add(0)
+print(set_1)
+set_1.add('G')
+print(set_1)
+
+print('G' in set_1)
+
---
+++ INDEX/4_Data_collections/3_Dictionaries.py
@@ -0,0 +1,11 @@
+data = {1,2,3,4}
+print(type(data))
+
+colours = {'amarillo': 'yellow', 'azul':'blue', 'verde': 'green'}
+
+print(colours['azul'])
+
+colours['amarillo']= 'white'
+# print(colours)
+del(colours['amarillo'])
+print(colours)
