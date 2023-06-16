a41e8bb00c1b3228e7769f016d5531f7ca6827d4
puldavid87 <puldavid87@gmail.com>
2023-06-16 19:20:51 +0200

    Unit 4

--- INDEX/3_Flow_controllers/5_Enumerate.py
+++ INDEX/3_Flow_controllers/5_Enumerate.py
@@ -1,4 +1,10 @@
 elements = ["hi", 4, "bye", [1,2,3]]
-print("hola")
+
 list(enumerate(elements))
 print(list)
+
+for i in enumerate(elements):
+    print(i)
+
+for index, i in enumerate(elements):
+    print(index,i)   
