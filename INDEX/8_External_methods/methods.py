fb2cda988cf1c5a949a5af5b7100039ac3351016
puldavid87 <puldavid87@gmail.com>
2023-06-29 13:34:08 +0200

    Unit8

---
+++ INDEX/8_External_methods/methods.py
@@ -0,0 +1,20 @@
+
+
+def sum(num1,num2):
+    print(num1+num2)
+
+def mul(num1,num2):
+    print(num1*num2)
+
+class mathe():
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+
+    def power(num1,num2):
+        print(num1*num2*num2)
+
+if __name__ == '__main__':
+    sum()
+    mul()
+    mathe()
